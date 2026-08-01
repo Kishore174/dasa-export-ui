ff11df1ac66b35b2758b82a7c6c06b074bbe79e1
Kishore174 <skishore174@gmail.com>
2026-08-01 19:52:43 +0530

    Update SEO metadata, JSON-LD schema, open graph tags, manifest and logo favicon

--- src/components/ui/Hero.jsx
+++ src/components/ui/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ArrowRight, Download, Ship } from "lucide-react";
+import { ArrowRight, Download } from "lucide-react";
 import RequestEstimateForm from "./Requestestimateform";
 
 // const routes = [
